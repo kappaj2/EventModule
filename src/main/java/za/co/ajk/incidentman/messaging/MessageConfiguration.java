6f7efa7e160c3c2a68d3a3acb02e95fc3675f7cb
Andre <kappaj@gmail.com>
2017-11-06 17:20:02 +0200

    Starting common config in ConfigServer

--- src/main/java/za/co/ajk/incidentman/messaging/MessageConfiguration.java
+++
@@ -1,7 +0,0 @@
-package za.co.ajk.incidentman.messaging;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-@EnableBinding(ConsumerChannels.class)
-public class MessageConfiguration {
-}
